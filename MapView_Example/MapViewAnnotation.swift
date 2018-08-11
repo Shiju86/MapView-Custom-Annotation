4ce79e7c27700f99597c0326257b7e10aae2eabe
Shiju Varghese <shiju86.v@gmail.com>
2018-08-11 13:19:12 +0530

    Add custom annotation

---
+++ MapView_Example/MapViewAnnotation.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewAnnotation.swift
+//  MapView_Example
+//
+//  Created by Shiju Varghese on 11/08/18.
+//  Copyright © 2018 Shiju. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+final class MapViewAnnotation: NSObject, MKAnnotation {
+  
+  var coordinate: CLLocationCoordinate2D
+  var title: String?
+  var subTitle: String?
+  
+  init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+    self.coordinate = coordinate
+    self.title = title
+    self.subTitle = subtitle
+    
+    super.init()
+  }
+  
+  var region: MKCoordinateRegion {
+    let span = MKCoordinateSpanMake(0.05, 0.05)
+    return MKCoordinateRegion(center: coordinate, span: span)
+  }
+  
+}
+
--- MapView_Example/ViewController.swift
+++ MapView_Example/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var mapView: MKMapView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+
+    mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+    
+    let cordinate = CLLocationCoordinate2D(latitude: 30.7047, longitude: 76.8003)
+    let annotation = MapViewAnnotation(coordinate: cordinate, title: "Airtel", subtitle: "Bharti Airtel Limited")
+    
+    mapView.addAnnotation(annotation)
+    mapView.setRegion(annotation.region, animated: true)
+    
+   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
+
+extension ViewController: MKMapViewDelegate {
+  
+  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    if let mapAnotationView = self.mapView.dequeueReusableAnnotationView(withIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier) as? MKMarkerAnnotationView {
+      
+      mapAnotationView.animatesWhenAdded = true
+      mapAnotationView.titleVisibility = .visible
+      return mapAnotationView
+    }
+    
+    return nil
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
